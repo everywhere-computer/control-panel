0e8dc17df1f6144e7b0d62ae65d9e05a1223221e
avivash <av@andrewvivash.com>
2023-10-20 15:31:04 -0700

    Feat: create FunctionCard

--- src/routes/functions/+page.svelte
+++ src/routes/functions/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { functionsStore } from '$src/stores'
   import Search from '$components/common/Search.svelte'
+  import FunctionCard from '$routes/functions/components/FunctionCard.svelte'
 
   let searchTerm = ''
   $: functions = searchTerm
@@ -23,30 +24,7 @@
 
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-x-4 sm:gap-y-8">
     {#each functions as func}
-      <div
-        class="flex flex-col border border-neutral-700 bg-odd-gray-0 text-odd-gray-500 transition-colors hover:bg-odd-blue-100"
-      >
-        <a href={`/functions/${func?.id}`}>
-          <div class="p-6">
-            <h3 class="mb-4 font-semibold text-heading-m">{func?.name}</h3>
-
-            <div class="flex flex-col items-center text-body-sm w-full mt-auto">
-              <div class="flex flex-row items-center justify-between w-full">
-                <p class="text-odd-gray-300 font-[550]">Version</p>
-                <p>{func?.version}</p>
-              </div>
-              <div class="flex flex-row items-center justify-between w-full">
-                <p class="text-odd-gray-300 font-[550]">Used by</p>
-                <p>{func?.numberOfProjectsUsing} workflows</p>
-              </div>
-              <div class="flex flex-row items-center justify-between w-full">
-                <p class="text-odd-gray-300 font-[550]">Last published</p>
-                <p>{func?.lastModifiedTime}</p>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
+      <FunctionCard {func} />
     {/each}
   </div>
 </div>
---
+++ src/routes/functions/components/FunctionCard.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { Func } from '$lib/functions'
+
+  export let func: Func
+</script>
+
+<div
+  class="flex flex-col border border-neutral-700 bg-odd-gray-0 text-odd-gray-500 transition-colors hover:bg-odd-blue-100"
+>
+  <a href={`/functions/${func?.id}`}>
+    <div class="p-6">
+      <h3 class="mb-4 font-semibold text-heading-m">{func?.name}</h3>
+
+      <div class="flex flex-col items-center text-body-sm w-full mt-auto">
+        <div class="flex flex-row items-center justify-between w-full">
+          <p class="text-odd-gray-300 font-[550]">Version</p>
+          <p>{func?.version}</p>
+        </div>
+        <div class="flex flex-row items-center justify-between w-full">
+          <p class="text-odd-gray-300 font-[550]">Used by</p>
+          <p>{func?.numberOfProjectsUsing} workflows</p>
+        </div>
+        <div class="flex flex-row items-center justify-between w-full">
+          <p class="text-odd-gray-300 font-[550]">Last published</p>
+          <p>{func?.lastModifiedTime}</p>
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
