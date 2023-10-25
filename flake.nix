f68f49e696c998bf97733d802adbd9cac73f227c
James Walker <walkah@walkah.net>
2023-10-25 16:27:08 -0400

    add a simple nix flake / shell (#8)

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "everywhere.computer control panel";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "eccp";
+          buildInputs = with pkgs; [ nodejs ];
+        };
+      }
+    );
+
+}
