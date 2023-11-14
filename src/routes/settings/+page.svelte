786be85f1484aaab8e70514375c03ed466fa39f9
avivash <av@andrewvivash.com>
2023-11-13 17:22:49 -0800

    Chore: add animation to settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
+  import { fly } from 'svelte/transition'
+
   import { sessionStore } from '$lib/stores'
   import AvatarUpload from '$components/settings/AvatarUpload.svelte'
   import ConnectedDevices from '$components/settings/ConnectedDevices.svelte'
@@ -10,6 +12,11 @@
 
 {#if $sessionStore.session}
   <div
+    in:fly={{
+      y: 20,
+      duration: 200,
+      opacity: 0
+    }}
     class="flex flex-col items-start max-w-[800px] m-auto gap-10 pb-28 text-sm"
   >
     <div class="flex flex-col items-start justify-center gap-6">
