6ad52af8e81341f4c1f2c3c327ea04bde4dec118
avivash <av@andrewvivash.com>
2023-11-15 13:02:01 -0800

    Chore: add OG image

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -1,6 +1,6 @@
 export const appName = 'EC Control Panel'
 export const appDescription = 'Control panel for managing workflows, function, payments, nodes, etc...'
 export const appURL = 'https://ec-control-panel-0.fission.app/'
-export const appImageURL = `${appURL}/preview.png`
+export const appImageURL = `${appURL}/homestar_mascot.png`
 export const ipfsGatewayUrl = 'runfission.com'
 export const oddNamespace = { creator: 'Fission', name: 'IPVM-Dashboard' }
