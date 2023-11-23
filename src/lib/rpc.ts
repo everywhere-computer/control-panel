55673d293166269b3b056c93b3f786ba944ddfad
avivash <av@andrewvivash.com>
2023-11-23 14:27:08 -0800

    Chore: fix up type mismatches from homestar SDK

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,11 +1,12 @@
+import type { Homestar as Client } from '@fission-codes/homestar'
 import { Homestar } from '@fission-codes/homestar'
 import { WebsocketTransport } from '@fission-codes/homestar/transports/ws.js'
 import { WebSocket } from 'unws'
 
 import { addNotification } from './notifications'
 
-let homestar
-export const getHomestarClient = () => {
+let homestar: Client
+export const getHomestarClient = (): Client => {
   try {
     if (!homestar) {
       const transport = new WebsocketTransport(
