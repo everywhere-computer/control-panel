9bd38dfd7ed4c3c46d9dae8a19a814c465e30f3a
avivash <av@andrewvivash.com>
2024-01-03 16:13:16 -0800

    Fix: remove leftover plausible code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -90,16 +90,6 @@
   })
 </script>
 
-<svelte:head>
-  {#if window?.location?.href?.includes('control')}
-    <script
-      defer
-      data-domain="control.everywhere.computer"
-      src="https://plausible.io/js/script.js"
-    ></script>
-  {/if}
-</svelte:head>
-
 <svelte:window bind:innerWidth={screenSize} />
 
 <div
