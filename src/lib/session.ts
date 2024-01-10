adce0aa4c9dd5d6c2a5493bc5a6dd646d77a6877
avivash <av@andrewvivash.com>
2024-01-10 10:28:28 -0800

    Chore: clean up capabilities function

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -22,38 +22,38 @@ export const IDB_UCAN_LABEL = 'control-panel/v1/ucan'
  * Fetch capabilities from fission-server
  * Note: this will be used once we have passkey auth enabled
  */
-export const getCapabilities = async (): Promise<void> => {
-  const serverDid = await (
-    await fetch('http://localhost:3000/api/v0/server-did')
-  ).text()
-  const audience = DIDKey.fromString(serverDid)
+export const getCapabilities = async (): Promise<{ revoked: string[]; ucans: {[did: string]: string} }> => {
+  try {
+    const serverDid = await (
+      await fetch('http://localhost:3000/api/v0/server-did')
+    ).text()
+    const audience = DIDKey.fromString(serverDid)
 
-  const principal = await RSASigner.import(
-    await localforage.getItem(IDB_PRIVATE_KEY_LABEL)
-  )
-  const pucan = await UCAN.create({
-    issuer: principal,
-    audience,
-    ttl: 60, // A rough estimate that accounts for clock drift
-    capabilities: ({
-      [principal.did.toString()]: { 'capability/fetch': [{}] }
-    } as unknown) as Capabilities
-  })
+    const principal = await RSASigner.import(
+      await localforage.getItem(IDB_PRIVATE_KEY_LABEL)
+    )
+    const pucan = await UCAN.create({
+      issuer: principal,
+      audience,
+      ttl: 60, // A rough estimate that accounts for clock drift
+      capabilities: ({
+        [principal.did.toString()]: { 'capability/fetch': [{}] }
+      } as unknown) as Capabilities
+    })
 
-  const res = await fetch('http://localhost:3000/api/v0/capabilities', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${pucan.toString()}`
-    }
-  })
+    const res = await fetch('http://localhost:3000/api/v0/capabilities', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${pucan.toString()}`
+      }
+    })
 
-  const capabilitiesRes = await res.json()
-  console.log('principal did', principal.did.toString())
-  console.log('capabilities res', capabilitiesRes)
-  console.log(
-    'capabilities res ucan count',
-    Object.keys(capabilitiesRes.ucans).length
-  )
+    const capabilitiesRes = await res.json()
+
+    return capabilitiesRes
+  } catch (error) {
+    console.error(error)
+  }
 }
