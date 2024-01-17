30896cc2afe07acc5b5bf78c22bf62f2d8df65fc
avivash <av@andrewvivash.com>
2024-01-17 11:49:50 -0800

    Feat: poll for online discord members

--- src/routes/onboarding/discord/+page.svelte
+++ src/routes/onboarding/discord/+page.svelte
@@ -1,7 +1,45 @@
 <script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
+
   import { sessionStore } from '$lib/stores'
   import StarMedium from '$components/icons/StarMedium.svelte'
   import StarSmall from '$components/icons/StarSmall.svelte'
+
+  const INTERVAL = 7500
+  let discordMemberPoll: NodeJS.Timer
+  let discordMembersOnline: number
+
+  // Start polling for online discord members
+  onMount(async () => {
+    const fetchMemberCount = async () => {
+      const res = await fetch(
+        'https://control-panel-online-discord-members.fission-cdd.workers.dev/',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+
+      discordMembersOnline = await res.json()
+      return discordMembersOnline
+    }
+    try {
+      await fetchMemberCount()
+
+      discordMemberPoll = setInterval(async () => {
+        await fetchMemberCount()
+      }, INTERVAL)
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  // Stop polling for online discord members
+  onDestroy(() => {
+    clearInterval(discordMemberPoll)
+  })
 </script>
 
 <div class="flex flex-col gap-4 md:gap-12 min-h-[calc(100vh-88px)]">
@@ -46,13 +84,18 @@
     <div
       class="flex flex-row items-center justify-center font-mono !font-bold text-code-sm bg-odd-green-50 border border-odd-green-400 py-3 rounded-sm"
     >
-      <span class="w-4 h-4 mr-2 bg-odd-green-400 rounded-full" />
-      9 MEMBERS ONLINE
+      <span
+        class="w-4 h-4 mr-2 {discordMembersOnline
+          ? 'bg-odd-green-400 animate-pulse'
+          : 'bg-odd-red-400'} rounded-full"
+      />
+      {discordMembersOnline ?? 'loading'} MEMBERS ONLINE
     </div>
 
     <div class="flex flex-col md:flex-row md:items-center gap-6">
       <a
-        href="/onboarding/nav"
+        href="https://discord.com/channels/478735028319158273/1182384404887244860"
+        target="_blank"
         class="btn btn-primary btn-odd-purple-500 self-center w-full md:max-w-[311px] h-10 !text-label-l"
       >
         Join the Discord
